2dc81bec02312d21e2d69731538344827a3a8405
suranjanz <72920235+suranjanz@users.noreply.github.com>
2026-06-26 15:36:19 +0100

    errorfix

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: uQj9iNy5 */
+/* Manifest version: JLMnAb6Y */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
